acbc73195ee55911bc88ffd407275212e24a5164
Nasir Khan Shimul <68707319+nasirkhnshimul@users.noreply.github.com>
2022-06-30 19:42:19 +0600

    Update config.py

--- config.py
+++ config.py
@@ -3,9 +3,9 @@ HEROKU = True # Make it False if you're not deploying on heroku.
 if HEROKU:
     from os import environ
 
-    bot_token = environ[5581650746:AAFk8ZB91GZZw4_uCFJVcDPjtS1FZpPltW0]
-    ARQ_API_KEY = environ[PLUROS-OYFPZG-AVPUCL-TNODRH-ARQ]
-    LANGUAGE = environ[bn]
+    bot_token = "5581650746:AAFk8ZB91GZZw4_uCFJVcDPjtS1FZpPltW0"
+    ARQ_API_KEY = "environ[PLUROS-OYFPZG-AVPUCL-TNODRH-ARQ"
+    LANGUAGE = "bn"
 
 # NOTE Fill this if you are not deploying on heroku.
 if not HEROKU:
